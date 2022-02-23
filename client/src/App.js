75337d42d4830ce9a26061e9b264fa6e94379251
Brian Cabrera <briancabrer4@gmail.com>
2022-02-23 17:54:13 -0300

    join component complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar.jsx"
 import Landing from "./Components/Landing/Landing.jsx"
 import Features from "./Components/Features/Features.jsx"
 import Footer from "./Components/Footer/Footer.jsx"
+import Join from "./Components/Join/Join.jsx"
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Landing />
       <Features />
+      <Join />
       <Footer />
     </div>
   );
---
+++ client/src/Components/Join/Join.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./join.scss"
+
+export default function Join() {
+  return (
+    <section className="join">
+        <div className="wrapper">
+            <small>35.000+ already joined</small>
+            <h4>Stay up-to-date with what we're doing</h4>
+            <form action>
+                <input type="text" placeholder="Enter your email address" className="email"/>
+                <input type="submit" className="button" value="Contact Us"/>
+            </form>
+        </div>
+    </section>
+  )
+}
